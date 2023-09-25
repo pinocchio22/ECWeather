17779fbe5836eab5b1931c629663e8caced61d5a
p2glet <vviian.2@gmail.com>
2023-09-25 20:34:41 +0900

    added ViewController

---
+++ ECWeather/Scene/RegionalPage/RegionalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegionalViewController.swift
+//  ECWeather
+//
+//  Created by t2023-m0056 on 2023/09/25.
+//
+
+import UIKit
+
+class RegionalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
