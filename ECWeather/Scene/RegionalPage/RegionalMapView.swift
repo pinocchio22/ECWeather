6c92ae537f9feb643243c4006ac9fc6f401425a0
p2glet <vviian.2@gmail.com>
2023-09-26 11:41:01 +0900

    [FEAT] : ⚙️ setting ReginalMapView

--- ECWeather/Scene/RegionalPage/RegionalMapView.swift
+++ ECWeather/Scene/RegionalPage/RegionalMapView.swift
@@ -14,6 +14,8 @@ class MapView: UIView {
     var myLocationButton: UIButton = {
         var btn = UIButton()
         btn.setTitle("현재 위치로", for: .normal)
+        btn.backgroundColor = .darkGray
+        btn.setTitleColor(.yellow, for: .normal)
         return btn
     }()
     
@@ -24,9 +26,27 @@ class MapView: UIView {
         
         self.addSubview(map)
         self.addSubview(myLocationButton)
+        
+        configureUI()
+        makeConstraintUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func configureUI() {
+    }
+    
+    func makeConstraintUI() {
+        map.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        myLocationButton.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(50)
+        }
+    }
+    
 }
