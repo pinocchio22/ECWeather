19a679bbd398226d1a8a0d3742a144f3acc45600
sanc93 <sanc93@me.com>
2023-09-26 13:56:33 +0900

    [FEAT] : ⚙️ 화면명 Label 추가

--- ECWeather/Scene/AlarmPage/AlarmViewController.swift
+++ ECWeather/Scene/AlarmPage/AlarmViewController.swift
@@ -5,25 +5,37 @@
 //  Created by t2023-m0056 on 2023/09/25.
 //
 
+import SnapKit
 import UIKit
 
 class AlarmViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "알람"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    private let timePicker: UIPickerView! = nil // TODO: - 시간 피커
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods & Selectors
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
     }
-    */
-
 }
