9e2b910f954c2ab3f742a72b5773362e9f69e29c
이시영 <jltldud@gmail.com>
2023-09-26 20:06:10 +0900

    branch is created

--- ECWeather/Scene/MainPage/MainViewController.swift
+++ ECWeather/Scene/MainPage/MainViewController.swift
@@ -14,6 +14,29 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    func weatherNowFrame(){
+    // Auto layout, variables, and unit scale are not yet supported
+    var view = UIView()
+    view.frame = CGRect(x: 0, y: 0, width: 345, height: 59)
+    view.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+
+    var parent = self.view!
+    parent.addSubview(view)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.widthAnchor.constraint(equalToConstant: 345).isActive = true
+    view.heightAnchor.constraint(equalToConstant: 59).isActive = true
+    view.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 15).isActive = true
+    view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 195).isActive = true
+    }
+    
+    
+    
+    
 
 }
 
