0070991d0dbc0d1afb15166379d59cfe1e569cbd
riyeonlee <gjffod100@gmail.com>
2023-09-27 20:31:44 +0900

    [FEAT] : ⚙️ Cell 추가

--- ECWeather/Scene/WeeklyPage/WeeklyViewController.swift
+++ ECWeather/Scene/WeeklyPage/WeeklyViewController.swift
@@ -28,6 +28,7 @@
 //
 //}
 
+
 import UIKit
 
 class WeeklyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
@@ -38,22 +39,26 @@ class WeeklyViewController: UIViewController, UITableViewDataSource, UITableView
         return tableView
     }()
     
+    struct WeatherData {
+        let day: String
+        let weather: String
+        let highTemperature: Int
+        let lowTemperature: Int
+    }
     
-    let weeklyForecast = [
-        ("Monday", "Sunny"),
-        ("Tuesday", "Cloudy"),
-        ("Wednesday", "Rainy"),
-        ("Thursday", "Partly Cloudy"),
-        ("Friday", "Sunny"),
-        ("Saturday", "Rainy"),
-        ("Sunday", "Sunny")
+    let weeklyForecast: [WeatherData] = [
+        WeatherData(day: "월요일", weather: "맑음", highTemperature: 28, lowTemperature: 15),
+        WeatherData(day: "화요일", weather: "흐림", highTemperature: 24, lowTemperature: 17),
+        WeatherData(day: "수요일", weather: "비", highTemperature: 20, lowTemperature: 14),
+        WeatherData(day: "목요일", weather: "국지적 흐림", highTemperature: 27, lowTemperature: 18),
+        WeatherData(day: "금요일", weather: "쨍쨍", highTemperature: 30, lowTemperature: 19),
+        WeatherData(day: "토요일", weather: "비", highTemperature: 21, lowTemperature: 15),
+        WeatherData(day: "일요일", weather: "쨍쨍", highTemperature: 29, lowTemperature: 16)
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Weekly Weather Forecast"
-        
         view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
@@ -65,7 +70,9 @@ class WeeklyViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(WeeklyTableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.separatorStyle = .singleLine
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,11 +80,69 @@ class WeeklyViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! WeeklyTableViewCell
         
-        let (day, weather) = weeklyForecast[indexPath.row]
-        cell.textLabel?.text = "\(day): \(weather)"
+        let weatherData = weeklyForecast[indexPath.row]
+        cell.configure(day: weatherData.day, weather: weatherData.weather, highTemperature: weatherData.highTemperature, lowTemperature: weatherData.lowTemperature)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let totalSpacingHeight = 16 * 2
+        let cellSpacing = 8
+        let cellHeight = 50
+        return CGFloat(cellHeight + cellSpacing + totalSpacingHeight)
+    }
+}
+
+class WeeklyTableViewCell: UITableViewCell {
+    
+    let dayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let weatherLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(dayLabel)
+        addSubview(weatherLabel)
+        addSubview(temperatureLabel)
+        
+        NSLayoutConstraint.activate([
+            dayLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            dayLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            
+            weatherLabel.leadingAnchor.constraint(equalTo: dayLabel.trailingAnchor, constant: 16),
+            weatherLabel.centerYAnchor.constraint(equalTo: dayLabel.centerYAnchor),
+            
+            temperatureLabel.leadingAnchor.constraint(equalTo: weatherLabel.trailingAnchor, constant: 16),
+            temperatureLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            temperatureLabel.centerYAnchor.constraint(equalTo: dayLabel.centerYAnchor),
+        ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(day: String, weather: String, highTemperature: Int, lowTemperature: Int) {
+        dayLabel.text = day
+        weatherLabel.text = weather
+        temperatureLabel.text = "\(highTemperature)° / \(lowTemperature)°"
     }
 }
