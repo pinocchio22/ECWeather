4651de345e58549e2ecd19c4c8fc2fd21bf77859
sanc93 <sanc93@me.com>
2023-09-27 14:28:35 +0900

    [REFACTOR] : ♻️ UIColor Extension 사용

--- ECWeather/Scene/AlarmPage/AlarmViewController.swift
+++ ECWeather/Scene/AlarmPage/AlarmViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 class AlarmViewController: UIViewController {
     
     // MARK: - Properties
-    private let weekdays:[String] = ["일","월","화","수","목","금","토",]
+    private let weekdays:[String] = ["일","월","화","수","목","금","토"]
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "날씨 알림"
         label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
-        label.textColor = UIColor(red: 0.00, green: 0.80, blue: 1.00, alpha: 1.00)
+        label.textColor = .ECWeatherColor3
         return label
     }()
     
@@ -25,8 +25,8 @@ class AlarmViewController: UIViewController {
         pickerView.datePickerMode = .time
         pickerView.preferredDatePickerStyle = .wheels
         pickerView.locale = Locale(identifier: "en_US")
-        pickerView.tintColor = UIColor(red: 0.02, green: 0.23, blue: 0.31, alpha: 1.00) // TODO: - tintColor 안먹음..
-        pickerView.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 0.3)
+        pickerView.tintColor = .ECWeatherColor3 // TODO: - tintColor 안먹음..
+        pickerView.backgroundColor = .ECWeatherColor4?.withAlphaComponent(0.3)
         return pickerView
     }()
 
@@ -34,7 +34,7 @@ class AlarmViewController: UIViewController {
         let label = UILabel()
         label.text = "요일별 알림"
         label.font = UIFont.boldSystemFont(ofSize: 10)
-        label.textColor = UIColor(red: 0.00, green: 0.80, blue: 1.00, alpha: 1.00)
+        label.textColor = .ECWeatherColor3
         return label
     }()
     
@@ -72,7 +72,7 @@ class AlarmViewController: UIViewController {
         
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.equalToSuperview().offset(20)
+            $0.leading.equalToSuperview().offset(25)
         }
         
         timePicker.snp.makeConstraints {
@@ -105,22 +105,13 @@ class AlarmViewController: UIViewController {
             button.setTitle(day, for: .normal)
             button.setTitleColor(.black, for: .normal)
             button.titleLabel?.textAlignment = .center
-            button.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 0.3)
+            button.backgroundColor = .ECWeatherColor4?.withAlphaComponent(0.3)
             button.addTarget(self, action: #selector(weekdaysButtonTapped), for: .touchUpInside)
             
             button.bounds = CGRect(x: 0, y: 0, width: 25, height: 50) // TODO: - 원형 만들기
             button.layer.cornerRadius = 0.5 * button.bounds.size.width
             
             weekdaysBtnStack.addArrangedSubview(button)
-        }
-    }
-
-    @objc private func weekdaysButtonTapped(sender: UIButton) {
-        if sender.backgroundColor == UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 0.3) {
-            sender.backgroundColor = UIColor(red: 0.00, green: 0.80, blue: 1.00, alpha: 0.5)
-            // TODO: - 요일 눌렀을때 이벤트 처리
-        } else {
-            sender.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 0.3)
         }
     }
     
@@ -131,6 +122,16 @@ class AlarmViewController: UIViewController {
         tableView.isScrollEnabled = false
         tableView.layer.cornerRadius = 10
         tableView.layer.masksToBounds = true
+        tableView.separatorStyle = .none
+    }
+    
+    @objc private func weekdaysButtonTapped(sender: UIButton) {
+        if sender.backgroundColor == .ECWeatherColor4?.withAlphaComponent(0.3) {
+            sender.backgroundColor = .ECWeatherColor3?.withAlphaComponent(0.5)
+            // TODO: - 요일 눌렀을때 이벤트 처리
+        } else {
+            sender.backgroundColor = .ECWeatherColor4?.withAlphaComponent(0.3)
+        }
     }
 }
 
@@ -147,7 +148,7 @@ extension AlarmViewController: UITableViewDataSource, UITableViewDelegate {
 //        
         let headerLabel = UILabel()
         headerLabel.font = UIFont.boldSystemFont(ofSize: 10)
-        headerLabel.textColor = UIColor(red: 0.00, green: 0.80, blue: 1.00, alpha: 1.00)
+        headerLabel.textColor = .ECWeatherColor3
         headerView.addSubview(headerLabel)
         
         if section == 0 {
@@ -178,12 +179,14 @@ extension AlarmViewController: UITableViewDataSource, UITableViewDelegate {
 
         cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
         cell.textLabel?.textColor = .black
-        cell.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 0.3)
+        cell.backgroundColor = .ECWeatherColor4?.withAlphaComponent(0.3)
         cell.selectionStyle = .none
+
 
         if indexPath.section == 0 {
             if indexPath.row == 0 {
                 cell.textLabel?.text = "알람 소리"
+                cell.layer.cornerRadius = 10
             }
         } else if indexPath.section == 1 {
             if indexPath.row == 0 {
@@ -191,10 +194,21 @@ extension AlarmViewController: UITableViewDataSource, UITableViewDelegate {
             } else if indexPath.row == 1 {
                 cell.textLabel?.text = "온도" // 현재 밖에 날씨는 ~~(18)도이고 체감온도는 ~~(25)입니다.
             }
+            cell.layer.cornerRadius = 10
         }
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            print("알람 소리 cell 터치 이벤트 들어옴!!")
+        } else if indexPath.section == 1 && indexPath.row == 0 {
+            print("날씨 cell 터치 이벤트 들어옴!! ")
+        } else if indexPath.section == 1 && indexPath.row == 1 {
+            print("온도 cell 터치 이벤트 들어옴!! ")
+        }
+    }
     
 }
+
