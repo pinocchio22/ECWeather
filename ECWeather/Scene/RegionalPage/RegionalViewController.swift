32aac0ab503015c3a7f5610bac56ebb1096d4c2a
p2glet <vviian.2@gmail.com>
2023-10-03 18:07:08 +0900

    [REFACTOR] : ♻️ deleted CL code in RegionalViewController

--- ECWeather/Scene/RegionalPage/RegionalViewController.swift
+++ ECWeather/Scene/RegionalPage/RegionalViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import MapKit
-// import CoreLocation
 import SnapKit
 import UIKit
 
@@ -14,11 +13,7 @@ class RegionalViewController: BaseViewController {
     let viewModel = RegionalViewModel()
     
     let mapView = RegionalMapView()
-    
-    let locationManager = CLLocationManager()
-    var latitude: Double?
-    var longitude: Double?
-    
+
     lazy var locationList:Array<CustomAnnotation> = []
     
     override func loadView() {
@@ -27,18 +22,11 @@ class RegionalViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 권한요청
-        locationManager.requestWhenInUseAuthorization()
+
         mapView.map.delegate = self
-        locationManager.delegate = self
-        
-        locationManager.startUpdatingLocation()
-        
+
         getLoactionWeather()
-        
         addCustomPin()
-        
         buttonActions()
     }
     
@@ -143,77 +131,11 @@ class RegionalViewController: BaseViewController {
         mapView.myLocationButton.addTarget(self, action: #selector(findMyLocation), for: .touchUpInside)
     }
     
-    func goSetting() {
-        let alert = UIAlertController(title: "위치권한 요청", message: "항상 위치 권한이 필요합니다.", preferredStyle: .alert)
-        let settingAction = UIAlertAction(title: "설정", style: .default) { _ in
-            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url)
-            }
-        }
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in
-        }
-        
-        alert.addAction(settingAction)
-        alert.addAction(cancelAction)
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func checkCurrentLocationAuthorization(authorizationStatus: CLAuthorizationStatus) {
-        switch authorizationStatus {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
-        case .restricted:
-            print("restricted")
-            goSetting()
-        case .denied:
-            print("denided")
-            goSetting()
-        case .authorizedAlways:
-            print("always")
-        case .authorizedWhenInUse:
-            print("wheninuse")
-            locationManager.startUpdatingLocation()
-        @unknown default:
-            print("unknown")
-        }
-        if #available(iOS 14.0, *) {
-            let accuracyState = locationManager.accuracyAuthorization
-            switch accuracyState {
-            case .fullAccuracy:
-                print("full")
-            case .reducedAccuracy:
-                print("reduced")
-            @unknown default:
-                print("Unknown")
-            }
-        }
-    }
-    
-    func checkUserLocationServicesAuthorization() {
-        let authorizationStatus: CLAuthorizationStatus
-        if #available(iOS 14, *) {
-            authorizationStatus = locationManager.authorizationStatus
-        } else {
-            authorizationStatus = CLLocationManager.authorizationStatus()
-        }
-        
-        if CLLocationManager.locationServicesEnabled() {
-            checkCurrentLocationAuthorization(authorizationStatus: authorizationStatus)
-        }
-    }
-    
     @objc func findMyLocation() {
-        guard locationManager.location != nil else {
-            locationManager.requestWhenInUseAuthorization()
-            return
-        }
         mapView.map.showsUserLocation = true
         mapView.map.setUserTrackingMode(.follow, animated: true)
         
-        self.viewModel.getMyLocationAnnotation(latitude: latitude!, longitude: longitude!) { item in
+        self.viewModel.getMyLocationAnnotation(latitude: DataManager.shared.latitude!, longitude: DataManager.shared.longitude!) { item in
             self.mapView.map.addAnnotation(item!)
         }
     }
@@ -246,25 +168,6 @@ extension RegionalViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         
     }
-}
-
-extension RegionalViewController: CLLocationManagerDelegate {
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        print(#function)
-        checkUserLocationServicesAuthorization()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        print(#function)
-        checkUserLocationServicesAuthorization()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if let location = locations.last {
-                self.latitude = location.coordinate.latitude
-                self.longitude = location.coordinate.longitude
-            }
-        }
 }
 
 extension RegionalViewController: UITableViewDelegate, UITableViewDataSource {
